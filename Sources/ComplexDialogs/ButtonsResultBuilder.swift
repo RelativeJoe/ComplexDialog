75a65877ac858e3a9bfec56cf60cb5e8ca56c5fe
Noe Jupiter <noeonjupiter@icloud.com>
2022-07-12 22:06:54 +0300

    Button Fixes 5

--- Sources/ComplexDialogs/ButtonsResultBuilder.swift
+++ Sources/ComplexDialogs/ButtonsResultBuilder.swift
@@ -9,20 +9,35 @@ import SwiftUI
 
 @resultBuilder
 public struct ButtonsResultBuilder {
-    @ViewBuilder public static func buildBlock<Content: View>(_ parts: Content...) -> some View {
+    @ViewBuilder public static func buildBlock<Content: View>(_ parts: DialogButton<Content>...) -> some View {
         ForEach(0..<parts.count) { index in
             let part = Array(parts)[index]
             let isLast = Array(parts).count == (index - 1) && parts.count > 1
             let isTop = index == 0  && parts.count > 1
             VStack {
-                part
-                    .padding(17)
-                    .padding(.bottom, isLast ? 0: -6)
-                    .padding(.top, isLast || !isTop ? -6: 0)
+                Button(action: {
+                    part.action()
+                }) {
+                    part.label
+                        .padding(17)
+                        .padding(.bottom, isLast ? 0: -6)
+                        .padding(.top, isLast || !isTop ? -6: 0)
+                }
                 if !isLast && parts.count > 1 {
                     Divider()
                 }
             }
         }
     }
+}
+
+public extension View {
+    func dialogAction(_ action: @escaping () -> Void) -> DialogButton<Self> {
+        DialogButton(action: action, label: self)
+    }
+}
+
+public struct DialogButton<Content: View> {
+    public var action: () -> Void
+    public var label: Content
 }
